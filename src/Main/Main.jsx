d0e900a2725c09c62c962f905ed9f4aca8e36fc9
roksana-barna <roksanabarna1803@gmail.com>
2023-05-18 11:48:45 +0600

    first commit

---
+++ src/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Components/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Main;
