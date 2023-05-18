61c0c9dd59c3c8fab7742b3fba28088c3c48d37a
roksana-barna <roksanabarna1803@gmail.com>
2023-05-18 13:06:53 +0600

    footer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from"../../assets/babypop.png";
+import logo from "../../assets/babypop.png";
 
 const Header = () => {
     const nav =
@@ -10,40 +10,35 @@ const Header = () => {
             <li><Link to='/mytoys'>My Toys</Link></li>
             <li><Link to='/addatoys'>Add a Toys</Link></li>
             <li><Link to='/blog'>Blog</Link></li>
-
-
         </div>
-
-
-
     return (
         <div className='w-9/12 mx-auto'>
             <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {nav}
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {nav}
 
+                        </ul>
+                    </div>
+                    <Link to={"/"} className="btn btn-ghost normal-case text-xl">
+                        <img className='h-10 w-10' src={logo} alt="" />
+                        <h2 className="text-xl ml-2 text-pink-500">BaBy pUPuS</h2>
+
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {nav}
                     </ul>
                 </div>
-                <Link to={"/"} className="btn btn-ghost normal-case text-xl">
-                    <img className='h-10 w-10' src={logo} alt="" />
-                    <h2 className="text-xl ml-2 text-pink-500">BaBy pUPuS</h2>
-
-                </Link>
+                <div className="navbar-end">
+                    <button className="btn bg-pink-400">Login</button>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {nav}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <button className="btn bg-pink-400">Login</button>
-            </div>
-        </div>
         </div>
 
     );
